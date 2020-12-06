81667e203580aaae50bec7440e14d295fe0ab92b
mirkonikic <4370557@gmail.com>
2020-12-06 23:23:40 +0100

    automated git commit

---
+++ js/projects.js
@@ -0,0 +1,13 @@
+//PROJECTS CHANGE BACKGROUND IMAGE
+document.getElementById("mircloud").addEventListener("mouseover", function() {
+    document.getElementById("projects-main-title").style.backgroundImage = "url(imgs/projects/micloud.png)";
+}, false);
+document.getElementById("mdos").addEventListener("mouseover", function() {
+    document.getElementById("projects-main-title").style.backgroundImage = "url(imgs/projects/mdos.png)";
+}, false);
+document.getElementById("mhtc").addEventListener("mouseover", function() {
+    document.getElementById("projects-main-title").style.backgroundImage = "url(imgs/projects/mhtc.png)";
+}, false);
+document.getElementById("m0_blog").addEventListener("mouseover", function() {
+    document.getElementById("projects-main-title").style.backgroundImage = "url(imgs/projects/m0_site.png)";
+}, false);
